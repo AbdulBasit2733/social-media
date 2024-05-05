87bb1ae7762a97932a804b15471fe05acdcf4a13
AbdulBasit2733 <abdulbasitkhan2733@gmail.com>
2024-05-05 14:56:10 +0530

    Added Loading Functionality

--- src/Components/Posts/Post.jsx
+++ src/Components/Posts/Post.jsx
@@ -2,14 +2,13 @@ import React, { useContext } from "react";
 import { AiFillDelete } from "react-icons/ai";
 import { PostList } from "../../store/post-list-store";
 const Post = ({ post }) => {
-
-
-  const {deletePost} = useContext(PostList)
+  const { deletePost } = useContext(PostList);
 
   return (
-    <div className="card" style={{ width: "30rem" }}>
+    <div className="card" style={{ minWidth: "16vw", maxWidth: "30rem" }}>
       <img src="..." className="card-img-top" alt="..." />
       <div className="card-body">
+        <h5 className="card-text">{post.id}</h5>
         <h5 className="card-title">
           {post.title}
           <span className="position-absolute top-0 start-100 translate-middle badge rounded-pill bg-danger">
@@ -24,7 +23,7 @@ const Post = ({ post }) => {
           </a>
         ))}
         <div className="alert alert-success reactions" role="alert">
-         This Post has been reacted by {post.reactions} peaple!
+          This Post has been reacted by {post.reactions} peaple!
         </div>
       </div>
     </div>
--- src/Components/Posts/PostList.jsx
+++ src/Components/Posts/PostList.jsx
@@ -1,27 +1,39 @@
-import React, { useContext, useEffect } from "react";
+import React, { useContext, useEffect, useState } from "react";
 import Post from "./Post";
 import { PostList as PostListData } from "../../store/post-list-store";
 import WelcomeMessage from "../Message/WelcomeMessage";
+import LoadingSpinner from "../Loading/LoadingSpinner";
 
 const PostList = () => {
   const { postList, addInitialPosts } = useContext(PostListData);
+  // console.log(postList);
+  const [fetching, setIsFetching] = useState(false);
 
   useEffect(() => {
+    setIsFetching(true);
+    console.log("Fetch Stated");
     fetch("https://dummyjson.com/posts")
       .then((res) => res.json())
       .then((data) => {
         addInitialPosts(data.posts);
+        setIsFetching(false);
+        console.log("Fetch Returned");
       });
+    console.log("Fetch Ended");
   }, []);
 
   return (
     <>
-      <div className="cards-container">
-        {postList.length === 0 && <WelcomeMessage />}
-        {postList.map((post) => (
-          <Post key={post.id} post={post} />
-        ))}
-      </div>
+      {fetching && <LoadingSpinner />}
+
+      {!fetching && (
+        <div className="cards-container">
+          {postList.length === 0 && <WelcomeMessage />}
+          {postList.map((post) => (
+            <Post key={post.id} post={post} />
+          ))}
+        </div>
+      )}
     </>
   );
 };
